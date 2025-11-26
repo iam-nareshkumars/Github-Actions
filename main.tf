89823d294167cb0e3a32901dd22c08cf34f3ef70
naresh <Naresh@gmail.com>
2025-11-26 21:45:13 +0530

    deleted demo old file

--- main.tf
+++ main.tf
@@ -1,3 +1,17 @@
-resource "aws_s3_bucket" "main" {
-  bucket = "tf-githubactions-bucket-26112025"
-}
+resource "aws_instance" "main" {
+ami           = "ami-07a0101b4055e3920" # us-east-1 devops-practice AMi
+instance_type = "t3.medium" 
+    tags = {
+    Name = "test"
+    }
+
+vpc_security_group_ids = ["sg-02efeff1df99019a6"]
+
+
+  ebs_block_device {
+    device_name = "/dev/sda1"
+    volume_size = 30
+  }
+    }
+ 
+
