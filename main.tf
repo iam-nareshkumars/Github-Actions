30285e125a1876c3f6d14aadc1af305ab0732655
naresh <Naresh@gmail.com>
2025-11-28 21:31:50 +0530

    terraform modified main file

--- main.tf
+++ main.tf
@@ -1,20 +1,11 @@
-resource "aws_launch_template" "example" {
-  name_prefix   = "my-template-"
-  image_id      = "ami-07a0101b4055e3920"  # replace with your AMI
-  instance_type = "t3.micro"
-                # optional: replace if you have a key
-
-  network_interfaces {
-    associate_public_ip_address = true
-    security_groups             = ["sg-02efeff1df99019a6"] # replace
+resource "aws_instance" "my_ec2" {
+  launch_template {
+    id      = "lt-01469ce463faed971"
+    version = "2"
   }
 
-  tag_specifications {
-    resource_type = "instance"
-
-    tags = {
-      Name = "LT-Managed-Instance"
-    }
+  tags = {
+    Name = "MyEC2FromTemplate"
   }
 }
 
