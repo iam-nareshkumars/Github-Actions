b08f643cfaf9a2f6a5f66fdac66972d715d0406d
naresh <Naresh@gmail.com>
2025-11-28 21:00:12 +0530

    terraform modified main file

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "my_ec2" {
   launch_template {
-    id      = "lt-0ddc9c5f4c9eccc94"    # your launch template ID
-    version = "$Latest"                # or specific version like "3"
+    id      = "lt-01469ce463faed971"    # your launch template ID
+    version = "2"                # or specific version like "3"
   }
 
   tags = {
